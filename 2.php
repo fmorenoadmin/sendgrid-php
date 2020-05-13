c01bf35994b2176f794fba753a4777deae04eb72
Frank Martin Moreno Alburqueque <admin@frankmorenoalburqueque.com>
2020-05-13 11:27:39 -0500

    Update 2.php

--- 2.php
+++ 2.php
@@ -1,15 +1,9 @@
 <?php
-require 'vendor/autoload.php'; // If you're using Composer (recommended)
-// Comment out the above line if not using Composer
-// Comment out the above line if not using Composer
-// require("<PATH TO>/sendgrid-php.php");
-// If not using Composer, uncomment the above line and
-// download sendgrid-php.zip from the latest release here,
-// replacing <PATH TO> with the path to the sendgrid-php.php file,
-// which is included in the download:
-// https://github.com/sendgrid/sendgrid-php/releases
+require 'vendor/autoload.php';
 
-$apiKey = 'SECRET_KEY';
+require 'constant.php';
+
+$apy_key=SECRET_KEY;
 $sg = new \SendGrid($apiKey);
 
 try {
